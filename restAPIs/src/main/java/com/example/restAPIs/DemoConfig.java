b648ca2aa2cdbfb433a473fac874701adec56c20
utk-pydev <utkarshvashisth94@gmail.com>
2023-09-22 01:07:01 +0530

    New features added

--- restAPIs/src/main/java/com/example/restAPIs/DemoConfig.java
+++ restAPIs/src/main/java/com/example/restAPIs/DemoConfig.java
@@ -1,22 +1,29 @@
 package com.example.restAPIs;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.logging.Logger;
 @Configuration
 public class DemoConfig {
 
-    private static Logger logger = (Logger) LoggerFactory.getLogger(DemoConfig.class);
+    private static Logger logger = LoggerFactory.getLogger(DemoConfig.class);
 
 
     //It will store it in the IOC container and no new object is created again for this. We can use it when we want to use in thr future also.
-    @Bean
+    @Bean("myBean")
     public RestTemplate getTemplate(){
         RestTemplate restTemplate = new RestTemplate();
-        logger.info("Inside getTemplate... {}");
+        logger.info("Inside getTemplate... {} Bean");
+        logger.info(restTemplate.toString());
+        return restTemplate;
+    }
+    @Bean("myNewBean")
+    public RestTemplate getTemplate2(){
+        RestTemplate restTemplate = new RestTemplate();
+        logger.info("Inside getTemplate... {} new Bean");
         logger.info(restTemplate.toString());
         return restTemplate;
     }
--- restAPIs/src/main/java/com/example/restAPIs/DemoController2.java
+++ restAPIs/src/main/java/com/example/restAPIs/DemoController2.java
@@ -3,9 +3,11 @@ package com.example.restAPIs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/v1")
@@ -15,9 +17,16 @@ public class DemoController2 {
     @Autowired
     Demo demo;
 
+
+    //synchronized --> critical section
+    ///application context --> ioc container
+
     @Autowired
     DemoController demoController;
-
+//If two beans of same return type but with different bean name then we can use @Qualifier to distinguish which bean name to use
+    @Autowired
+    @Qualifier("myBean")
+    RestTemplate restTemplate;
     private static final Logger logger = LoggerFactory.getLogger(DemoController2.class);
     @GetMapping("/sample")
     public Demo getDemo(){
