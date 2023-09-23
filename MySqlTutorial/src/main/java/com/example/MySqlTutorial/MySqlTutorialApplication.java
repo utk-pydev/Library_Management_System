141d9c5d6bd08cd19dc0be92ad3d95b23efacfa8
utk-pydev <utkarshvashisth94@gmail.com>
2023-09-23 15:30:19 +0530

    MySQl tutorial in SpringBoot

---
+++ MySqlTutorial/src/main/java/com/example/MySqlTutorial/MySqlTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySqlTutorial;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlTutorialApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySqlTutorialApplication.class, args);
+	}
+
+}
---
+++ MySqlTutorial/src/test/java/com/example/MySqlTutorial/MySqlTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySqlTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
